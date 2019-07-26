f0b8871f47f69167978d7bdadc4095523d153415
zjade <zjade@outlook.com>
2019-07-26 10:39:25 +0800

    TravisCI swift_version Tests

---
+++ ValidationTests/ValidationTests.swift
@@ -0,0 +1,33 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Apple on 2019/7/26.
+//  Copyright © 2019 zjade. All rights reserved.
+//
+
+import XCTest
+
+class ValidationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
